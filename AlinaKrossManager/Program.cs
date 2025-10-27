452a9df920ca3b4d2d8e638d76b3cb01e69d470b
Yushko_YM <Yushko_YM@ST.BY>
2025-10-27 17:34:57 +0300

    тест 333

--- AlinaKrossManager/Program.cs
+++ AlinaKrossManager/Program.cs
@@ -11,6 +11,9 @@ builder.Services.AddOpenApi();
 // Регистрация Telegram Bot
 builder.Services.AddSingleton<ITelegramBotClient>(provider =>
 {
+	var test = Environment.GetEnvironmentVariable("TelegramBotToken");
+	Console.WriteLine(test);
+
 	var token = "8169570733:AAH6HBzf2Nv8k9XjldVZ5QMm_fRJDGz-r6g";//builder.Configuration["TelegramBot:Token"];
 	return new TelegramBotClient(token);
 });
