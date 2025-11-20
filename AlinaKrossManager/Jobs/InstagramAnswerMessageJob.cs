a75427102a73bfe9c95025f3d7181055e03a9c04
Yushko_YM <Yushko_YM@ST.BY>
2025-11-20 20:11:05 +0300

    fdfdf

--- AlinaKrossManager/Jobs/InstagramAnswerMessageJob.cs
+++ AlinaKrossManager/Jobs/InstagramAnswerMessageJob.cs
@@ -50,7 +50,7 @@ namespace AlinaKrossManager.Jobs
 
 				Console.WriteLine("webRootPath = " + webRootPath);
 
-				// 2. Создаем подпапку temp_audio 
+				// 2. Создаем подпапку temp_audio ds
 				var tempFolder = Path.Combine(webRootPath, "temp_audio");
 				if (!Directory.Exists(tempFolder))
 				{
